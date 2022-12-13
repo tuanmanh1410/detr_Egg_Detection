cfb96081862db9dfa5d23410007129e05c318bc9
tuanmanh1410 <tuanmanh.bka@gmail.com>
2022-12-14 02:02:04 +0900

    Update code for run notebook

--- models/detr.py
+++ models/detr.py
@@ -313,7 +313,7 @@ def build(args):
     
     # num_classes = 20 if args.dataset_file != 'coco' else 91
     # num_classes = 91  # With Mini COCO dataset:wq
-    num_classes = 9 # COLOR DATASET | 9 classes for MONO DATASET
+    num_classes = 91 # COLOR DATASET | 9 classes for MONO DATASET
     if args.dataset_file == "coco_panoptic":
         # for panoptic, we just add a num_classes that is large enough to hold
         # max_obj_id + 1, but the exact value doesn't really matter
